67d603dacd945e2d864b31bf77369235d7e24f2a
Omal Perera <omalperera8@gmail.com>
2016-01-31 11:42:11 +0530

    Server Socket is working perfectly

---
+++ Form_Server_Socket.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace ServerSocketApp
+{
+    partial class Form_Server_Socket
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_container = new System.Windows.Forms.Panel();
+            this.lbl_server_msg_show = new System.Windows.Forms.Label();
+            this.lbl_rercieved_msg = new System.Windows.Forms.Label();
+            this.lbl_server_status = new System.Windows.Forms.Label();
+            this.btn_start_server = new System.Windows.Forms.Button();
+            this.btn_start_listen = new System.Windows.Forms.Button();
+            this.panel_container.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel_container
+            // 
+            this.panel_container.BackColor = System.Drawing.Color.White;
+            this.panel_container.Controls.Add(this.btn_start_listen);
+            this.panel_container.Controls.Add(this.lbl_server_msg_show);
+            this.panel_container.Controls.Add(this.lbl_rercieved_msg);
+            this.panel_container.Controls.Add(this.lbl_server_status);
+            this.panel_container.Controls.Add(this.btn_start_server);
+            this.panel_container.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_container.Location = new System.Drawing.Point(0, 0);
+            this.panel_container.Name = "panel_container";
+            this.panel_container.Size = new System.Drawing.Size(486, 302);
+            this.panel_container.TabIndex = 0;
+            // 
+            // lbl_server_msg_show
+            // 
+            this.lbl_server_msg_show.AutoSize = true;
+            this.lbl_server_msg_show.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.lbl_server_msg_show.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(3)))), ((int)(((byte)(45)))));
+            this.lbl_server_msg_show.Location = new System.Drawing.Point(146, 200);
+            this.lbl_server_msg_show.Name = "lbl_server_msg_show";
+            this.lbl_server_msg_show.Size = new System.Drawing.Size(91, 21);
+            this.lbl_server_msg_show.TabIndex = 6;
+            this.lbl_server_msg_show.Text = "nothing got";
+            // 
+            // lbl_rercieved_msg
+            // 
+            this.lbl_rercieved_msg.AutoSize = true;
+            this.lbl_rercieved_msg.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.lbl_rercieved_msg.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(3)))), ((int)(((byte)(45)))));
+            this.lbl_rercieved_msg.Location = new System.Drawing.Point(28, 200);
+            this.lbl_rercieved_msg.Name = "lbl_rercieved_msg";
+            this.lbl_rercieved_msg.Size = new System.Drawing.Size(117, 21);
+            this.lbl_rercieved_msg.TabIndex = 5;
+            this.lbl_rercieved_msg.Text = "Received Msg : ";
+            // 
+            // lbl_server_status
+            // 
+            this.lbl_server_status.AutoSize = true;
+            this.lbl_server_status.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.lbl_server_status.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(3)))), ((int)(((byte)(45)))));
+            this.lbl_server_status.Location = new System.Drawing.Point(28, 129);
+            this.lbl_server_status.Name = "lbl_server_status";
+            this.lbl_server_status.Size = new System.Drawing.Size(131, 21);
+            this.lbl_server_status.TabIndex = 4;
+            this.lbl_server_status.Text = "Server Status : off";
+            // 
+            // btn_start_server
+            // 
+            this.btn_start_server.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(2)))), ((int)(((byte)(55)))));
+            this.btn_start_server.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_start_server.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_start_server.FlatAppearance.BorderSize = 0;
+            this.btn_start_server.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.btn_start_server.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.btn_start_server.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_start_server.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_start_server.ForeColor = System.Drawing.Color.White;
+            this.btn_start_server.Location = new System.Drawing.Point(174, 22);
+            this.btn_start_server.Name = "btn_start_server";
+            this.btn_start_server.Size = new System.Drawing.Size(165, 29);
+            this.btn_start_server.TabIndex = 3;
+            this.btn_start_server.Text = "Start the Server";
+            this.btn_start_server.UseVisualStyleBackColor = false;
+            this.btn_start_server.Click += new System.EventHandler(this.btn_conct_server_Click);
+            // 
+            // btn_start_listen
+            // 
+            this.btn_start_listen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(2)))), ((int)(((byte)(55)))));
+            this.btn_start_listen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_start_listen.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btn_start_listen.FlatAppearance.BorderSize = 0;
+            this.btn_start_listen.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.btn_start_listen.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.btn_start_listen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_start_listen.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_start_listen.ForeColor = System.Drawing.Color.White;
+            this.btn_start_listen.Location = new System.Drawing.Point(174, 68);
+            this.btn_start_listen.Name = "btn_start_listen";
+            this.btn_start_listen.Size = new System.Drawing.Size(165, 29);
+            this.btn_start_listen.TabIndex = 7;
+            this.btn_start_listen.Text = "Start Listning";
+            this.btn_start_listen.UseVisualStyleBackColor = false;
+            this.btn_start_listen.Click += new System.EventHandler(this.btn_start_listen_Click);
+            // 
+            // Form_Server_Socket
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 302);
+            this.Controls.Add(this.panel_container);
+            this.Name = "Form_Server_Socket";
+            this.Text = "Server Socket";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel_container.ResumeLayout(false);
+            this.panel_container.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_container;
+        private System.Windows.Forms.Label lbl_server_msg_show;
+        private System.Windows.Forms.Label lbl_rercieved_msg;
+        private System.Windows.Forms.Label lbl_server_status;
+        private System.Windows.Forms.Button btn_start_server;
+        private System.Windows.Forms.Button btn_start_listen;
+    }
+}
+
---
+++ Form_Server_Socket.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerSocketApp
+{
+    public partial class Form_Server_Socket : Form
+    {
+
+
+        SocketPermission permission;
+        Socket sListener;
+        IPEndPoint ipEndPoint;
+        Socket handler;
+
+        private TextBox tbAux = new TextBox();
+
+
+
+
+        public Form_Server_Socket()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_conct_server_Click(object sender, EventArgs e)
+        {
+            start_the_server();
+
+        }
+
+        private void btn_start_listen_Click(object sender, EventArgs e)
+        {
+            listning_mode();
+        }
+
+
+
+
+
+
+        private void start_the_server()
+        {
+            try
+            {
+                // Creates one SocketPermission object for access restrictions
+                permission = new SocketPermission(
+                NetworkAccess.Accept,     // Allowed to accept connections 
+                TransportType.Tcp,        // Defines transport types 
+                "",                       // The IP addresses of local host 
+                SocketPermission.AllPorts // Specifies all ports 
+                );
+
+                // Listening Socket object 
+                sListener = null;
+
+                // Ensures the code to have permission to access a Socket 
+                permission.Demand();
+
+                // Resolves a host name to an IPHostEntry instance 
+                IPHostEntry ipHost = Dns.GetHostEntry("");
+
+                // Gets first IP address associated with a localhost 
+                IPAddress ipAddr = ipHost.AddressList[0];
+
+                // Creates a network endpoint 
+                ipEndPoint = new IPEndPoint(ipAddr, 4610);
+
+                // Create one Socket object to listen the incoming connection 
+                sListener = new Socket(
+                    ipAddr.AddressFamily,
+                    SocketType.Stream,
+                    ProtocolType.Tcp
+                    );
+
+                // Associates a Socket with a local endpoint 
+                sListener.Bind(ipEndPoint);
+
+                lbl_server_status.Text = "Server started Successfully.";
+            }
+            catch (Exception exc) { MessageBox.Show(exc.ToString()); }
+        }
+
+        private void listning_mode()
+        {
+            try
+            {
+                // Places a Socket in a listening state and specifies the maximum 
+                // Length of the pending connections queue 
+                sListener.Listen(10);
+
+                // Begins an asynchronous operation to accept an attempt 
+                AsyncCallback aCallback = new AsyncCallback(AcceptCallback);
+                sListener.BeginAccept(aCallback, sListener);
+
+                lbl_server_status.Text = "Server is listening on " + ipEndPoint.Address + " port: " + ipEndPoint.Port;
+
+
+                //Send_Button.IsEnabled = true;
+            }
+            catch (Exception exc) { MessageBox.Show(exc.ToString()); }
+
+        }
+
+
+        public void AcceptCallback(IAsyncResult ar)
+        {
+            Socket listener = null;
+
+            // A new Socket to handle remote host communication 
+            Socket handler = null;
+            try
+            {
+                // Receiving byte array 
+                byte[] buffer = new byte[1024];
+                // Get Listening Socket object 
+                listener = (Socket)ar.AsyncState;
+                // Create a new socket 
+                handler = listener.EndAccept(ar);
+
+                // Using the Nagle algorithm 
+                handler.NoDelay = false;
+
+                // Creates one object array for passing data 
+                object[] obj = new object[2];
+                obj[0] = buffer;
+                obj[1] = handler;
+
+                // Begins to asynchronously receive data 
+                handler.BeginReceive(
+                    buffer,        // An array of type Byt for received data 
+                    0,             // The zero-based position in the buffer  
+                    buffer.Length, // The number of bytes to receive 
+                    SocketFlags.None,// Specifies send and receive behaviors 
+                    new AsyncCallback(ReceiveCallback),//An AsyncCallback delegate 
+                    obj            // Specifies infomation for receive operation 
+                    );
+
+                // Begins an asynchronous operation to accept an attempt 
+                AsyncCallback aCallback = new AsyncCallback(AcceptCallback);
+                listener.BeginAccept(aCallback, listener);
+            }
+            catch (Exception exc) { MessageBox.Show(exc.ToString()); }
+        }
+
+
+        string content = string.Empty;
+        string str = string.Empty;
+        public void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Fetch a user-defined object that contains information 
+                object[] obj = new object[2];
+                obj = (object[])ar.AsyncState;
+
+                // Received byte array 
+                byte[] buffer = (byte[])obj[0];
+
+                // A Socket to handle remote host communication. 
+                handler = (Socket)obj[1];
+
+                // Received message 
+                //string content = string.Empty;
+
+
+                // The number of bytes received. 
+                int bytesRead = handler.EndReceive(ar);
+
+                if (bytesRead > 0)
+                {
+                    content += Encoding.Unicode.GetString(buffer, 0,
+                        bytesRead);
+
+                    // If message contains "<Client Quit>", finish receiving
+                    if (content.IndexOf("<Client Quit>") > -1)
+                    {
+                        // Convert byte array to string
+                        string str = content.Substring(0, content.LastIndexOf("<Client Quit>"));
+
+                        //this is used because the UI couldn't be accessed from an external Thread
+                        this.BeginInvoke(new InvokeDelegate(HandleSelection_2));
+                         
+                    }
+                    else
+                    {
+                        // Continues to asynchronously receive data
+                        byte[] buffernew = new byte[1024];
+                        obj[0] = buffernew;
+                        obj[1] = handler;
+                        handler.BeginReceive(buffernew, 0, buffernew.Length,
+                            SocketFlags.None,
+                            new AsyncCallback(ReceiveCallback), obj);
+                    }
+
+                    this.BeginInvoke(new InvokeDelegate(HandleSelection_1)); 
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.ToString());
+            }
+        }
+        public void HandleSelection_1()
+        {
+            lbl_server_msg_show.Text = content;
+
+        }
+
+        public void HandleSelection_2()
+        {
+            tbAux.Text = "Read " + str.Length * 2 + " bytes from client.\n Data: " + str;
+        }
+
+        public void SendCallback(IAsyncResult ar)
+        {
+
+            try
+            {
+                // A Socket which has sent the data to remote host 
+                Socket handler = (Socket)ar.AsyncState;
+
+                // The number of bytes sent to the Socket 
+                int bytesSend = handler.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to Client", bytesSend);
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch (Exception exc) { MessageBox.Show(exc.ToString()); }
+
+        }
+
+
+        public delegate void InvokeDelegate();
+        private void tbAux_SelectionChanged(object sender, EventArgs e)
+        {
+            this.BeginInvoke(new InvokeDelegate(HandleSelection));
+        }
+
+        private void HandleSelection()
+        {
+                lbl_server_msg_show.Text = tbAux.Text;
+        }
+    /*
+            private void button2_Click_1(object sender, EventArgs e)
+            {
+
+                try
+                {
+                    // Convert byte array to string 
+                    string str = txtB.Text;
+
+                    // Prepare the reply message 
+                    byte[] byteData =
+                        Encoding.Unicode.GetBytes(str);
+
+                    // Sends data asynchronously to a connected Socket 
+                    handler.BeginSend(byteData, 0, byteData.Length, 0,
+                        new AsyncCallback(SendCallback), handler);
+
+                    //Send_Button.IsEnabled = false;
+                    //Close_Button.IsEnabled = true;
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(exc.ToString());
+                }
+
+            }
+            */
+}
+}
